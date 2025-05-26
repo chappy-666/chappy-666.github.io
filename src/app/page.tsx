b46d70f3a091de0ab649dcc17e233268246bae3e
chappy <chappy.job.666@gmail.com>
2025-05-26 15:11:04 +0000

    add next.js files

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Chappy Portfolio Site!
+      </h1>
+    </div>
+  );
+}
